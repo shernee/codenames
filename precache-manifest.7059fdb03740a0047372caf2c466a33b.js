87ae2bad203554e14f80b5a2d209008baaa86dcc
Neetha Sherra <neethasherra@gmail.com>
2020-07-09 15:07:57 -0500

    Updates

---
+++ precache-manifest.7059fdb03740a0047372caf2c466a33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e16ef35e6584e186af4242b44bd73d",
+    "url": "/shernee/codenames/index.html"
+  },
+  {
+    "revision": "7e47a34a079b4a4a0edc",
+    "url": "/shernee/codenames/static/css/main.d4fd3a16.chunk.css"
+  },
+  {
+    "revision": "1726a4b3ddca3c86cd37",
+    "url": "/shernee/codenames/static/js/2.6cdfc3d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shernee/codenames/static/js/2.6cdfc3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e47a34a079b4a4a0edc",
+    "url": "/shernee/codenames/static/js/main.b64c5771.chunk.js"
+  },
+  {
+    "revision": "49ec95f6c78e20d3e717",
+    "url": "/shernee/codenames/static/js/runtime-main.e48bc0aa.js"
+  }
+]);
